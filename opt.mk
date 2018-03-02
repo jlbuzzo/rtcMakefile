577b56d9bc40beadebd1d7acb5b487665a2ca0d6
Jose Leonel Buzzo <jlbuzzo@pnd4.bioinfohsl.org.br>
2018-03-02 13:14:00 -0300

    Initial state.

---
+++ opt.mk
@@ -0,0 +1,8 @@
+REF_ANNOTATION:=/home/projects/databases/gencode/release26/gencode.v26.annotation.gtf
+REP_ANNOTATION:=/home/projects/local_collab/gh_RTC_simulation/make/library/annotation/rep.hg38.converted.bed
+REP_ANNOTATION_manual:=/home/projects/local_collab/gh_RTC_simulation/make/library/annotation/rep.hg38.converted.manual.bed
+PIPELINE_NAME:=retroCNVator
+DISTANCE:=750000
+MIN_DIST := 1000000
+TEMP_PROCESS_DIR := $(OUTPUT_DIR)/temp/
+DUMP_DIR := $(OUTPUT_DIR)/result/dump/
